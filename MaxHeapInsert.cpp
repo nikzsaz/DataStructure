102117494f66c7dc2d0e9dd2577ccbd214f53bcd
nikzsaz <mr.nirajjee@gmail.com>
2016-04-30 01:08:15 +0530

    Max Heap Insertion

---
+++ MaxHeapInsert.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int arr[10001];
+void swap(int a,int b){
+    a=a^b;
+    b=a^b;
+    a=a^b;
+}
+void insert(int num,int index){
+    int parent,temp;
+    if(index==0){
+        arr[index]=num;
+    }else{
+        arr[index]=num;
+        parent=(index-1)/2;
+        while(arr[index]>arr[parent] && parent>=0){
+            swap(arr[index],arr[parent]);
+            index=parent;
+            parent=(index-1)/2;
+        }
+    }
+}
+int main() {
+    int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	    int num;
+	    cin>>num;
+	    insert(num,i);
+	}
+	for(int i=0;i<n;i++){
+	    cout<<arr[i]<<" ";
+	}
+	return 0;
+}
