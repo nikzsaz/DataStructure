7203062b22fa5c409482420b4183f6e080367135
nikzsaz <mr.nirajjee@gmail.com>
2017-08-27 01:28:14 +0530

    N meeting in one room greedy approach

---
+++ interview/NmeetingInOneRoomGreedy.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+bool comp(pair<int,int> a,pair<int,int> b){
+    return (a.second < b.second);
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        pair<int,int> arr[n];
+        pair<int,int> arr1[n];
+        for(int i = 0 ; i < n ; i++){
+            cin>>arr[i].first;
+            arr1[i].first = arr[i].first;
+        }
+        
+        for(int i = 0 ; i < n ;i++){
+            cin>>arr[i].second;
+            arr1[i].second = arr[i].second;
+        }
+        
+        sort(arr,arr+n,comp);
+        for(int k = 0 ; k < n ;k++){
+        if((arr1[k].second == arr[0].second) && (arr1[k].first == arr[0].first))
+            cout<<k+1<<" ";
+        }
+        int count = 1;
+        int j = 0;
+        for(int i = 1; i < n; i++){
+            if(arr[j].second <= arr[i].first){
+                count++;
+                j = i;
+                 for(int k = 0 ; k < n ;k++){
+                   if((arr1[k].second == arr[j].second) && (arr1[k].first == arr[j].first)){
+                        cout<<k+1<<" ";
+                    } 
+                }
+            }
+        }
+        cout<<endl;
+        
+    }
+	return 0;
+}
