436124cf08238b260219ed9bcd4153b1d8d3ec52
Niraj Sazzie <mr.nirajjee@gmail.com>
2017-08-26 23:21:03 +0530

    Update ToCreateEdgeBetweenNode.js

--- interview/ToCreateEdgeBetweenNode.js
+++ interview/ToCreateEdgeBetweenNode.js
@@ -4,12 +4,12 @@ var arr2 = [34,5,4];
 var arr = new Object();
 
 for(var i = 0 ; i < 3 ; i++){
-	arr[i] = new Object();
+    arr[i] = new Object();
     arr[i]["first"] = arr1[i];
     arr[i]["second"] = arr2[i];
 }
 
 for(var i = 0 ; i < 3 ; i++){
-	document.write(arr[i].first + " - >  " +arr[i].second + "</br>");
+    document.write(arr[i].first + " - >  " +arr[i].second + "</br>");
 }
 
