35133737b1c6984f9a36c102c2f5b0d0c67e8c35
nikzsaz <mr.nirajjee@gmail.com>
2017-08-27 23:53:19 +0530

    Coinpile Greedy approach

---
+++ interview/CoinPileGreedy.ts
@@ -0,0 +1,83 @@
+[input]	
+
+1
+74 665
+142 712 254 869 548 645 663 758 38 860 724 742 530 779 317 36 191 843 289 107 41 943 265 649 447 806 891 730 371 351 7 102 394 549 630 624 85 955 757 841 967 377 932 309 945 440 627 324 538 539 119 83 930 542 834 116 640 659 705 931 978 307 674 387 22 746 925 73 271 830 778 574 98 513 
+
+[output]
+
+Initial count 0 after processing 4569 element is 7
+Initial count 7 after processing 4169 element is 22
+Initial count 29 after processing 3813 element is 36
+Initial count 65 after processing 3795 element is 38
+Initial count 103 after processing 3753 element is 41
+Initial count 144 after processing 3010 element is 73
+Initial count 217 after processing 2861 element is 83
+Initial count 300 after processing 2902 element is 85
+Initial count 385 after processing 2725 element is 98
+Initial count 483 after processing 2747 element is 102
+Initial count 585 after processing 2754 element is 107
+Initial count 692 after processing 2695 element is 116
+Initial count 808 after processing 2760 element is 119
+Initial count 927 after processing 2489 element is 142
+Initial count 1069 after processing 1923 element is 191
+Initial count 1260 after processing 1495 element is 254
+Initial count 1514 after processing 1655 element is 265
+Initial count 1779 after processing 1887 element is 271
+Initial count 2050 after processing 2088 element is 289
+Initial count 2339 after processing 2345 element is 307
+Initial count 2646 after processing 2650 element is 309
+Initial count 2955 after processing 2955 element is 317
+Initial count 3272 after processing 3272 element is 324
+Initial count 3596 after processing 3596 element is 351
+Initial count 3947 after processing 3947 element is 371
+Initial count 4318 after processing 4318 element is 377
+Initial count 4695 after processing 4695 element is 387
+Initial count 5082 after processing 5082 element is 394
+Initial count 5476 after processing 5476 element is 440
+Initial count 5916 after processing 5916 element is 447
+Initial count 6363 after processing 6363 element is 513
+Initial count 6876 after processing 6876 element is 530
+Initial count 7406 after processing 7406 element is 538
+Initial count 7944 after processing 7944 element is 539
+Initial count 8483 after processing 8483 element is 542
+Initial count 9025 after processing 9025 element is 548
+Initial count 9573 after processing 9573 element is 549
+Initial count 10122 after processing 10122 element is 574
+Initial count 10696 after processing 10696 element is 624
+Initial count 11320 after processing 11320 element is 627
+Initial count 11947 after processing 11947 element is 630
+Initial count 12577 after processing 12577 element is 640
+Initial count 13217 after processing 13217 element is 645
+Initial count 13862 after processing 13862 element is 649
+Initial count 14511 after processing 14511 element is 659
+Initial count 15170 after processing 15170 element is 663
+Initial count 15833 after processing 15833 element is 674
+Initial count 16507 after processing 16507 element is 705
+Initial count 17212 after processing 17212 element is 712
+Initial count 17924 after processing 17924 element is 724
+Initial count 18648 after processing 18648 element is 730
+Initial count 19378 after processing 19378 element is 742
+Initial count 20120 after processing 20120 element is 746
+Initial count 20866 after processing 20866 element is 757
+Initial count 21623 after processing 21623 element is 758
+Initial count 22381 after processing 22381 element is 778
+Initial count 23159 after processing 23159 element is 779
+Initial count 23938 after processing 23938 element is 806
+Initial count 24744 after processing 24744 element is 830
+Initial count 25574 after processing 25574 element is 834
+Initial count 26408 after processing 26408 element is 841
+Initial count 27249 after processing 27249 element is 843
+Initial count 28092 after processing 28092 element is 860
+Initial count 28952 after processing 28952 element is 869
+Initial count 29821 after processing 29821 element is 891
+Initial count 30712 after processing 30712 element is 925
+Initial count 31637 after processing 31637 element is 930
+Initial count 32567 after processing 32567 element is 931
+Initial count 33498 after processing 33498 element is 932
+Initial count 34430 after processing 34430 element is 943
+Initial count 35373 after processing 35373 element is 945
+Initial count 36318 after processing 36318 element is 955
+Initial count 37273 after processing 37273 element is 967
+Initial count 38240 after processing 38240 element is 978
+1495
---
+++ interview/CoinPileGreedyApproach.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+   int t;
+   cin>>t;
+   while(t--){
+       int count = 0;
+       int arr[100001];
+       int n,k;
+       int temp;
+       int final = 1000001;
+       cin>>n>>k;
+       for(int i = 0 ; i < n; i++){
+           cin>>arr[i];
+       }
+       sort(arr,arr+n);
+       int loop = 0;
+       for(int i = 0 ; i < n ; i++){
+           count = loop;
+           cout<<"Initial count "<<count;
+           for(int j = i+1 ; j < n ; j++ ){
+               if((arr[j]-arr[i]) > k ){
+                  count+=arr[j] - arr[i] - k;
+               }
+           }
+           cout<<" after processing "<<count<<" element is "<<arr[i]<<endl;
+           if(count < final)
+                final = count;
+            loop+=arr[i];
+       }
+       cout<<final<<endl;
+   }
+	return 0;
+}
